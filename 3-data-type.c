ce0ff6b86e417d750b71794f21c3e1d87cc1cc48
toyo30 <toyo30@naver.com>
2022-02-06 19:29:00 +0900

    add: lesson3

--- 3-data-type.c
+++ 3-data-type.c
@@ -1,29 +1,39 @@
-
+// sizeof()
+//int, 정수 float, 실수 double, 실수 초과된 메모리도 표시 char 문자형
 
 
 
 #include <stdio.h>
-#include <limits.h>
-//int_max 불러오기 위함. 
+#define MONTHS 12
+
+//달력 변수를 가져오기 위함.
 
 //#inclue <> 라이브러리를 추가하는 부분
-//라이브러리를 추가해야 print 사용 가
-int main(void)//int라는 자료형 사용함. 점수형을 반환하는 int/void 매계 변수. 
+
+int main(void)
 {
-   
+//    double monthSalary = 1000.5;
+//    printf("$ %.2f", monthSalary * MONTHS);
+   //$는 뭔가? 
+   //char x = 'A';
+   char x = 65;
+   printf("%c\n", x);
+   //내부적으로 숫자로 구성됨. 
+   char y = 66;
+   printf("%c\n", y);
 
-    //2 variable
+   char z = 66;
+   printf("%d\n", z);
 
-    int x = 10;
-    int y = 20;
-    printf("x= %d입니다. \n", x);
-    printf("y = %d입니다. \n", y);
 
-    
-    printf("x + y = %d입니다.\n", x + y);
-    printf("x - y = %d입니다.\n", x - y);
-    printf("x * y = %d입니다.\n", x * y);
-    printf("x / y = %d입니다.\n", x / y);
+
+    //int형을 다른 진수로 바꾸기 
+
+    int k = 100;
+    printf("10진수로 출력 : %d\n", k);
+    printf("8진수로 출력 : %o\n", k);
+    printf("16진수로 출력 : %x\n", k);
+
 
     return 0;
 }
