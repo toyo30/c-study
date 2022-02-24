d393a9a6319ac933e5da18f8d156ff90fd6ba399
toyo30 <toyo30@naver.com>
2022-02-24 20:58:32 +0900

    add: c-lang-arr

---
+++ 9-function.c
@@ -0,0 +1,16 @@
+
+/*
+#include <stdio.h>
+
+void main()
+{
+ mina += min
+ hour += min/60   
+}
+
+int main(void)
+{
+    int x;
+    scanf("%d", &x);//&(앰퍼센드, ampersand ,and + et)특정 변수의 메모리 주소자체를 의미함 
+}
+*/
---
+++ practice-arr.c
@@ -0,0 +1,79 @@
+// #include <stdio.h>
+// int main(void)
+// {
+//     int a,b,c;
+//     scanf("%d %d %d", &a,&b,&c);
+//     int numArr[3] = { a, b, c };
+//     printf("{%d %d %d}", numArr[0], numArr[1], numArr[2]);
+//     int i;
+//     //sizeof(int) 자료형의 크기로 나누어주는 것이 필요하다. 
+//     for(i = 0; i < sizeof(numArr)/sizeof(int); i++)
+//     {   
+//         if(numArr[i]%2 == 0)
+//         {
+//             printf("%d", numArr[i]);
+//         }
+//     }
+//     // for()
+//     return 0;
+// }
+
+
+/*
+
+#include <stdio.h>
+int main(void)
+{
+    long long int a,b,c;
+    scanf("%lld %lld %lld", &a,&b,&c);  
+    int num[3];
+    num[0] = a;
+    num[1] = b;
+    num[2] = c;
+    // int numArr[3] = {a, b, c};
+    // printf(numArr[3]);
+    for(int i = 0; i < sizeof(numArr)/sizeof(int); i++)
+    {
+        if(numArr[i]%2 == 0)
+        {
+            printf("%d", numArr[i]);
+        }
+    }
+    return 0;
+}
+
+
+*/
+
+
+#include <stdio.h>
+int main(void)
+{
+    long long int a;
+    scanf("%lld", &a);
+    if(a > 0)
+    {
+        printf("plus\n");
+        if(a % 2 == 0)
+        {
+            printf("even");
+        }
+        else if(a % 2 == 1)
+        {
+            printf("odd");
+        }
+    }
+    else if(a < 0)
+    {
+        printf("minus\n");
+        if(a % 2 == 0)
+        {
+            printf("even");
+        }
+        else if(a % 2 == 1)
+        {
+            printf("odd");
+        }
+    }
+    return 0;
+}
